9a06afa7f3632cee3674b554e940d3d2699839f1
luizgouveia <gouveia.luiz89@gmail.com>
2020-06-28 22:46:01 -0300

    Update deleta-aplicacao.sh

--- scripts/deleta-aplicacao.sh
+++ scripts/deleta-aplicacao.sh
@@ -1,2 +1,2 @@
-rm -rf /var/lib/tomcat8/webapps/alura-aws.war
-rm -rf /var/lib/tomcat8/webapps/alura-aws
+sudo rm -rf /var/lib/tomcat8/webapps/alura-aws.war
+sudo rm -rf /var/lib/tomcat8/webapps/alura-aws
